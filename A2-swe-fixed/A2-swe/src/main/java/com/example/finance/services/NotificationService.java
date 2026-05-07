413f59d1ed59a3d89fe6d0840975ee0666f215d7
Fk99-a11y <fk080641@gmail.com>
2026-05-07 14:56:02 +0300

    Add NotificationService to send budget alerts

---
+++ A2-swe-fixed/A2-swe/src/main/java/com/example/finance/services/NotificationService.java
@@ -0,0 +1,9 @@
+package com.example.finance.service;
+
+public class NotificationService {
+
+    public void sendBudgetAlert() {
+
+        System.out.println("Warning: Budget limit exceeded!");
+    }
+}
